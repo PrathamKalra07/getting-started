5ceb8d35b1a73cb34da355bb144361bce8ce8d1d
sudip-erudite <100074011+sudip-erudite@users.noreply.github.com>
2024-10-19 12:05:56 +0530

    fix: EWS_64 - brand logo fixed.

--- src/redux/slices/basicInfoReducer.js
+++ src/redux/slices/basicInfoReducer.js
@@ -6,6 +6,7 @@ export const basicInfoSlice = createSlice({
     uuid: "",
     uuidTemplateInstance: "",
     uuidSignatory: "",
+    salesforceOrgId: ""
   },
   reducers: {
     setInfo: (state, action) => {
@@ -15,10 +16,14 @@ export const basicInfoSlice = createSlice({
       state.uuidSignatory = uuidSignatory;
       state.uuidTemplateInstance = uuidTemplateInstance;
     },
+    setSalesforceOrgId: (state, action) => {
+      const { salesforceOrgId } = action.payload;
+      state.salesforceOrgId = salesforceOrgId;
+    }
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setInfo } = basicInfoSlice.actions;
+export const { setInfo, setSalesforceOrgId } = basicInfoSlice.actions;
 
 export default basicInfoSlice.reducer;
